a8cac908ca9b0fbe9325c1b814fb839bad4192e6
flqh <1691141895@qq.com>
2024-05-22 23:24:28 +0800

    test

---
+++ back/demo/spring-trans/spring-trans/src/test/java/com/example/springtrans/SpringTransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
